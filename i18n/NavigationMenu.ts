4bbe922034456b5066fdf69278f20ecd740551c4
gitlherme <dev@guilhermevieira.dev>
2022-04-14 19:14:50 -0300

    feat(i18n): add in menu links

---
+++ i18n/NavigationMenu.ts
@@ -0,0 +1,30 @@
+export const NavigationMenu = {
+  "pt-BR": [
+    {
+      "title": "Início",
+      "link": "/"
+    },
+    {
+      "title": "Sobre mim",
+      "link": "/about",
+    },
+    {
+      "title": "Blog",
+      "link": "/blog",
+    }
+  ],
+  "en-US": [
+    {
+      "title": "Home",
+      "link": "/"
+    },
+    {
+      "title": "About",
+      "link": "/about",
+    },
+    {
+      "title": "Blog",
+      "link": "/blog",
+    }
+  ]
+}
--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,12 +1,21 @@
 import { Flex } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { NavigationMenu as navLinks } from "../../i18n/NavigationMenu";
 import NavigationLink from "./NavigationLink";
 
+
 export function NavigationMenu() {
+  const { locale } = useRouter()
+  const localeLinks = navLinks[locale as keyof typeof navLinks]
   return (
     <Flex as="nav" gap="4" justify="center">
-      <NavigationLink href="/">Home</NavigationLink>
-      <NavigationLink href="/about">About me</NavigationLink>
-      <NavigationLink href="/blog">Blog</NavigationLink>
+      {
+        localeLinks.map(({ title, link }) => {
+          return (
+            <NavigationLink key={title} href={link}>{title}</NavigationLink>
+          )
+        })
+      }
     </Flex>
   )
 }
