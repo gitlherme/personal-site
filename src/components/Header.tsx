51eeee3f35a038857b82aa76a49f2ef4f3153b24
gitlherme <dev@guilhermevieira.dev>
2022-04-15 22:13:06 -0300

    feat(colorMode): add light and dark mode in mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,32 +10,35 @@ export function Header() {
     <Box>
       <Flex justify="space-between" py="0.5rem" display={["flex", "none"]}>
         <SocialMediaBlock />
-        <Menu>
-          <MenuButton as="button">
-            <Icon as={RiMenuLine} fontSize="24px" />
-          </MenuButton>
-          <MenuList
-            w="100vw"
-            h="30vh"
-            bg="black"
-            border="none"
-            borderRadius="none"
-            display="flex"
-            flexDirection="column"
-            justifyContent="space-between"
-          >
-            <MenuItem _hover={{ bg: "transparent" }} _focus={{ bg: "transparent" }}>
-              <NavigationMenu />
-            </MenuItem>
-            <MenuItem _hover={{ bg: "transparent" }} _focus={{ bg: "transparent" }}
-              w="100%"
+        <Flex gap="2rem">
+          <ToggleColorMode />
+          <Menu>
+            <MenuButton as="button">
+              <Icon as={RiMenuLine} fontSize="24px" />
+            </MenuButton>
+            <MenuList
+              w="100vw"
+              h="30vh"
+              bg="black"
+              border="none"
+              borderRadius="none"
               display="flex"
-              justifyContent="center"
+              flexDirection="column"
+              justifyContent="space-between"
             >
-              <Flags />
-            </MenuItem>
-          </MenuList>
-        </Menu>
+              <MenuItem _hover={{ bg: "transparent" }} _focus={{ bg: "transparent" }}>
+                <NavigationMenu />
+              </MenuItem>
+              <MenuItem _hover={{ bg: "transparent" }} _focus={{ bg: "transparent" }}
+                w="100%"
+                display="flex"
+                justifyContent="center"
+              >
+                <Flags />
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
       </Flex>
       <Flex h="48px" justify="space-between" py="0.5rem" display={["none", "flex"]} align="center">
         <NavigationMenu />
